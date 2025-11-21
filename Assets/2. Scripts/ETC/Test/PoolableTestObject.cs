d88a1a1add98f88d062dc14e4a6063c49de0087d
PSH <packtty18@naver.com>
2025-11-21 14:06:33 +0900

    Test : 풀링 테스트 씬, 코드 ,객체 생성

---
+++ Assets/2. Scripts/ETC/Test/PoolableTestObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PoolableTestObject : MonoBehaviour, IPoolable
+{
+    public void OnDespawn()
+    {
+        Debug.Log("despawn");
+        gameObject.SetActive(false);
+    }
+
+    public void OnSpawn()
+    {
+        Debug.Log("Spawn");
+        
+        gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/Editor/TestPoolEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TestPool))]
+public class TestPoolEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        TestPool pool = (TestPool)target;
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("Spawn Test Object"))
+        {
+            pool.SpawnTestObject();
+        }
+
+        if (GUILayout.Button("Return Test Object"))
+        {
+            pool.ReturnTestObject();
+        }
+    }
+}
