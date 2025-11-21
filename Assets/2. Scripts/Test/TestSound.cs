9ae21d46e0d1e9f23813a1c966a2b8eb3da20dd8
PSH <packtty18@naver.com>
2025-11-21 18:25:58 +0900

    Test : 테스트 코드 리펙토링

--- Assets/2. Scripts/Test/TestSound.cs
+++ Assets/2. Scripts/Test/TestSound.cs
@@ -14,41 +14,18 @@ public class TestSound : MonoBehaviour
     [ContextMenu("Play Debug Sound")]
     public void PlayDebugSound()
     {
-        SoundLibrarySO library = SoundManager.Instance.GetLibrary();
-        if (library == null)
+        if(!SoundManager.IsManagerExist())
         {
-            Debug.LogWarning("[SoundManager Debug] SoundLibrary is missing!");
             return;
         }
-
-        if (_debugSelectedType == ESoundType.None)
+        SoundManager sound = SoundManager.Instance;
+        if(_debugIsBGM)
         {
-            Debug.LogWarning("[SoundManager Debug] No sound type selected!");
-            return;
-        }
-
-        AudioClip clip = library.GetClip(_debugSelectedType);
-        if (clip == null)
-        {
-            Debug.LogWarning($"[SoundManager Debug] Clip not found for {_debugSelectedType}");
-            return;
-        }
-
-        if (_debugIsBGM)
-        {
-            SoundObject bgmSource = SoundManager.Instance.GetBgmSource();
-            // BGM용 사운드 오브젝트 생성 혹은 변경
-            if (bgmSource == null)
-            {
-                SoundManager.Instance.Init();
-            }
-            bgmSource.OnPlay(clip, true, _debugPlayTime);
+            sound.PlayBGM(_debugSelectedType, _debugPlayTime);
         }
         else
         {
-            GameObject soundObject = PoolManager.Instance.Spawn<SoundPool, ESoundObject>(ESoundObject.SoundObject);
-            var so = soundObject.GetComponent<SoundObject>();
-            so.OnPlay(clip, false, _debugPlayTime);
+            sound.PlaySFX(_debugSelectedType, _debugPlayTime);
         }
     }
 }
