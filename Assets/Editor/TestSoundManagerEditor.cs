d6eb6922739a39cc42cae92fee437f15c9089594
PSH <packtty18@naver.com>
2025-11-21 17:20:10 +0900

    Test : 사운드 테스트용 씬과 에디터 스크립트 추가

---
+++ Assets/Editor/TestSoundManagerEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SoundManager))]
+public class TestSoundManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        SoundManager soundManager = (SoundManager)target;
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("Play Sound"))
+        {
+            soundManager.PlayDebugSound();
+        }
+
+    }
+}
