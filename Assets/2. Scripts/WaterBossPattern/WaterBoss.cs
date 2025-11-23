dd1d152cd983ff23f964d9e5ffd95d98690e3bf6
erilsion <erilsion@gmail.com>
2025-11-23 17:55:40 +0900

    Feat: 물 보스 효과 관련 메서드 생성
    - 물 보스가 공격할 타이밍이 되면 왜곡 효과를 만들어내는 메서드를 생성했습니다.
    - 추후 왜곡 효과 생성 방법에 따라 수정될 수 있습니다.

--- Assets/2. Scripts/WaterBossPattern/WaterBoss.cs
+++ Assets/2. Scripts/WaterBossPattern/WaterBoss.cs
@@ -2,15 +2,55 @@ using UnityEngine;
 
 public class WaterBoss : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Material _rippleMaterial;  // Ripple 효과를 가진 Material
+    private float _rippleStrength = 1.0f;  // 효과의 세기
+    private float _rippleDuration = 3.0f;  // 효과의 지속 시간
+    private float _rippleDelayCoolTime = 5.0f;  // 효과 발동 전 대기 시간
+    private float _rippleStartTime;  // Ripple 효과 시작 시간
+    private bool _isRippleActive = false;  // 효과가 활성화된 상태
+    private bool _hasEffectStarted = false;  // 효과 발동 여부 추적
+
+
+    private void Update()
     {
-        
+        StartWaterAttack();
+        FinishWaterAttack();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 일정 시간이 되면 Ripple 효과를 활성화하는 메서드
+    private void StartWaterAttack()
     {
-        
+        if (!_hasEffectStarted && Time.time >= _rippleDelayCoolTime)
+        {
+            _hasEffectStarted = true;
+            WaterRippleEffect(true);
+            _rippleStartTime = Time.time;
+        }
+    }
+
+    // Ripple 효과 활성화 관련 메서드
+    private void WaterRippleEffect(bool isActive)
+    {
+        if (isActive)
+        {
+            // Ripple 효과 활성화
+            _rippleMaterial.SetFloat("_RippleStrength", _rippleStrength);
+            _isRippleActive = true;
+        }
+        else
+        {
+            // Ripple 효과 비활성화
+            _rippleMaterial.SetFloat("_RippleStrength", 0f);
+            _isRippleActive = false;
+        }
+    }
+
+    // 일정 시간이 지나면 Ripple 효과를 비활성화하는 메서드
+    private void FinishWaterAttack()
+    {
+        if (_isRippleActive && Time.time >= _rippleStartTime + _rippleDuration)
+        {
+            WaterRippleEffect(false);
+        }
     }
 }
