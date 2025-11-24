3f8bc03206b9a75c6ff149bbfeda5b4a204f4315
PSH <packtty18@naver.com>
2025-11-24 18:39:28 +0900

    Feat : 스텟 제작 완료

---
+++ Assets/2. Scripts/ScriptableObject/Data/UpgradeDatabaseSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UpgradeChoiceGroup
+{
+    public UpgradeOptionSO[] Options;
+}
+
+[CreateAssetMenu(fileName = "UpgradeDatabase", menuName = "SO/UpgradeDatabase")]
+public class UpgradeDatabaseSO : ScriptableObject
+{
+    [SerializeField] private List<UpgradeChoiceGroup> UpgradeChoice;
+
+}
---
+++ Assets/2. Scripts/ScriptableObject/Data/UpgradeOptionSO.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public enum EUpgradeType
+{
+    HealthUp,
+    DamageUp,
+    AttackGuageDown,
+    FeverGuageDown,
+    BuddyDamageDown,
+    BuddyHealthDown,
+    FeverAttackGuagePlusUp,
+    FeverDamagePlusUp,
+    HealUp,
+}
+
+[CreateAssetMenu(fileName = "UpgradeOption", menuName = "SO/UpgradeOption")]
+public class UpgradeOptionSO : ScriptableObject
+{
+    public EUpgradeType OptionType;
+    public float Value;
+
+    public void Apply(PlayerStatDataSO playerStat, BuddyStatDataSO buddyStat)
+    {
+        Debug.Log($"[Upgrade] Apply {OptionType} : {Value}");
+
+        switch (OptionType)
+        {
+            case EUpgradeType.HealthUp:
+                playerStat.MaxHealth += Value;
+                break;
+            case EUpgradeType.DamageUp:
+                playerStat.BaseDamage += Value;
+                break;
+            case EUpgradeType.AttackGuageDown:
+                playerStat.AttackGuage -= Value;
+                break;
+            case EUpgradeType.FeverGuageDown:
+                playerStat.FeverGuage -= Value;
+                break;
+            case EUpgradeType.BuddyDamageDown:
+                buddyStat.DamageRate -= Value;
+                break;
+            case EUpgradeType.BuddyHealthDown:
+                buddyStat.HealthRate -= Value;
+                break;
+            case EUpgradeType.FeverAttackGuagePlusUp:
+                playerStat.FeverAttackGuagePlus += Value;
+                break;
+            case EUpgradeType.FeverDamagePlusUp:
+                playerStat.FeverDamagePlus += Value;
+                break;
+            case EUpgradeType.HealUp:
+                playerStat.Heal += Value;
+                break;
+        }
+    }
+}
