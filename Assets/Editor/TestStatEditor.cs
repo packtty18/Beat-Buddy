4c8345e7d377ae0f16d7ac4af780ce35686e02d7
PSH <packtty18@naver.com>
2025-11-24 18:39:45 +0900

    Test : 테스트 스크립트 생성

---
+++ Assets/Editor/TestStatEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+[CustomEditor(typeof(StatManager))]
+public class TestStatEditor : Editor
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        StatManager testSound = (StatManager)target;
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("SetStat"))
+        {
+            testSound.TestSetStat();
+        }
+
+    }
+}
