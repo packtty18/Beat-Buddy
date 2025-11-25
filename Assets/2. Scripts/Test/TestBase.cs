65833e7fcccd453e4e80905945ad13502c08ac38
PSH <packtty18@naver.com>
2025-11-25 16:28:21 +0900

    Chore : 싱글톤 이름 변경 누락파일 추가

--- Assets/2. Scripts/Test/TestBase.cs
+++ Assets/2. Scripts/Test/TestBase.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class TestBase : SimpleSingleton<TestBase>
+public class TestBase : CoreSingleton<TestBase>
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
