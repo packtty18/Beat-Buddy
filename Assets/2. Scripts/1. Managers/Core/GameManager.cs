81cf94a2bbf3a5937c4f35bc277fac84395d0737
PSH <packtty18@naver.com>
2025-11-26 16:32:08 +0900

    Chore : 각 씬이동마다 트랜지션 타입 설정하도록 변경

--- Assets/2. Scripts/1. Managers/Core/GameManager.cs
+++ Assets/2. Scripts/1. Managers/Core/GameManager.cs
@@ -15,15 +15,14 @@ public class GameManager : CoreSingleton<GameManager>
         base.Awake();
     }
 
-    public void ChangeScene(ESceneType newScene)
+    public void ChangeScene(ESceneType newScene , ETransitionType outTransition, ETransitionType inTransition)
     {
-        ESceneType oldScene = _currentScene;
         _currentScene = newScene;
 
-        LoadScene(_currentScene);
+        LoadScene(_currentScene, outTransition , inTransition);
     }
 
-    private void LoadScene(ESceneType currentScene)
+    private void LoadScene(ESceneType currentScene, ETransitionType outTransition, ETransitionType inTransition)
     {
         Debug.Log($"[GameManager] Scene 로드 시작: {currentScene}");
         MySceneManager.Instance.LoadScene(currentScene, _outTransitionType, _inTransitionType, _blockLoadSameScene);
@@ -36,19 +35,19 @@ public class GameManager : CoreSingleton<GameManager>
 
     public void ReturnToSongSelect()
     {
-        ChangeScene(ESceneType.SongSelect);
+        ChangeScene(ESceneType.SongSelect, ETransitionType.ModeToSongOut, ETransitionType.ModeToSongIn);
     }
 
-    public void ReturnToLobby()
+    public void ReturnToMode()
     {
-        ChangeScene(ESceneType.Lobby);
+        ChangeScene(ESceneType.Lobby, ETransitionType.SongToModeOut, ETransitionType.SongToModeIn);
     }
     public void StartStage()
     {
         if (SongManager.Instance.SelectedSong != null)
         {
             Debug.Log($"[GameManager] 게임 시작 요청: {SongManager.Instance.GetSelectedSongName()}");
-            ChangeScene(ESceneType.Stage);
+            ChangeScene(ESceneType.Stage, ETransitionType.ModeToSongOut, ETransitionType.ModeToSongIn);
         }
         else
         {
